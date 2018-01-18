f933be59a491b90728fd23d4c2609549ef478d1b
nasre <nasre@LAPTOP-57NQC83A.fios-router.home>
2018-01-18 13:13:20 -0500

    Initial commit

---
+++ consumer/src/consumer/ConsumerInfo.java
@@ -0,0 +1,8 @@
+package consumer;
+
+public class ConsumerInfo {
+	public void display() {
+		System.out.println("Address : Main st, Ohio");
+	}
+
+}
---
+++ consumer/src/consumer/consumerreport.java
@@ -0,0 +1,10 @@
+package consumer;
+
+public class consumerreport {
+	public static void main(String args[]) {
+		ConsumerInfo ci = new ConsumerInfo();
+		System.out.println("name:Aijal");
+		ci.display();
+
+	}
+}
