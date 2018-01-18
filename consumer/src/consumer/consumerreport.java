bada230ac7df32c1548239e4c01c256beffc5159
nasre <nasre@LAPTOP-57NQC83A.fios-router.home>
2018-01-18 13:52:24 -0500

    Added Last name

--- consumer/src/consumer/consumerreport.java
+++ consumer/src/consumer/consumerreport.java
@@ -4,6 +4,7 @@ public class consumerreport {
 	public static void main(String args[]) {
 		ConsumerInfo ci = new ConsumerInfo();
 		System.out.println("name:Aijal");
+		System.out.println("lname:savy");
 		ci.display();
 
 	}
