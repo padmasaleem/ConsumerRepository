b87cddeef5dac09e18ddb12c297bba57d9dc5d2e
nasreen <nasre@LAPTOP-57NQC83A.fios-router.home>
2018-01-23 14:05:09 -0500

    created array

--- consumer/src/consumer/consumerreport.java
+++ consumer/src/consumer/consumerreport.java
@@ -3,8 +3,11 @@ package consumer;
 public class consumerreport {
 	public static void main(String args[]) {
 		ConsumerInfo ci = new ConsumerInfo();
-		System.out.println("name:Aijal");
-		System.out.println("lname:savy");
+		String[] names = { "Aijal", "Savy", "Binu" };
+		for (String s : names) {
+			System.out.println(s);
+		}
+
 		ci.display();
 
 	}
