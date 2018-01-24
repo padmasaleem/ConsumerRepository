d7067b3520ef2ddd76abd6d05df37d09a241f020
nasreen <nasre@LAPTOP-57NQC83A.fios-router.home>
2018-01-24 11:15:18 -0500

    changed for loop and added more values

--- consumer/src/consumer/consumerreport.java
+++ consumer/src/consumer/consumerreport.java
@@ -3,9 +3,9 @@ package consumer;
 public class consumerreport {
 	public static void main(String args[]) {
 		ConsumerInfo ci = new ConsumerInfo();
-		String[] names = { "Aijal", "Savy", "Binu" };
-		for (String s : names) {
-			System.out.println(s);
+		String[] names = { "Aijal", "Savy", "Binu", "lily", "amy" };
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i]);
 		}
 
 		ci.display();
