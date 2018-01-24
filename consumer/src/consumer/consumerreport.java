eddb17b74465772a5452b9210eaf4ef80617404f
nasreen <nasre@LAPTOP-57NQC83A.fios-router.home>
2018-01-24 11:21:17 -0500

    Added full name

--- consumer/src/consumer/consumerreport.java
+++ consumer/src/consumer/consumerreport.java
@@ -1,13 +1,19 @@
 package consumer;
-
+/**
+ * Print customer names
+ * @author nasre
+ *
+ */
 public class consumerreport {
 	public static void main(String args[]) {
 		ConsumerInfo ci = new ConsumerInfo();
 		String[] names = { "Aijal", "Savy", "Binu" };
+		String fullname = null;
 		for (String s : names) {
 			System.out.println(s);
+			fullname = s+" gold";
 		}
-
+		System.out.println(fullname);
 		ci.display();
 
 	}
